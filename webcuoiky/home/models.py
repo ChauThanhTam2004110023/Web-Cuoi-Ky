a627f75b2b4600825b34885bfe66c315f83e949b
ChauThanhTam2004110023 <90594941+ChauThanhTam2004110023@users.noreply.github.com>
2023-05-22 08:38:45 +0700

    nth-update

--- webcuoiky/home/models.py
+++ webcuoiky/home/models.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 
 #s ok
 class Customer(models.Model):
+    class Meta:
+        ordering = ['-id']
+        
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True)
     address = models.CharField(max_length=200, null=True)
@@ -19,6 +22,9 @@ class Customer(models.Model):
     
 # ok
 class Category(models.Model):
+    class Meta:
+        ordering = ['-id']
+
     name = models.CharField(max_length=200, null=False)
     images = models.ImageField(upload_to='category/%Y/%m', default=None)
 
@@ -27,13 +33,16 @@ class Category(models.Model):
 
 # ok
 class Product(models.Model):
+    class Meta:
+        ordering = ['-id']
+
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     descriptions = models.TextField(default=None)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     created_Date = models.DateTimeField(default=timezone.now)
-    images = models.ImageField(upload_to='product/%Y/%m', default=None)
+    images = models.ImageField(upload_to='product/%Y/%m', default=None)        
 
     def __str__(self):
         return self.name
@@ -50,6 +59,9 @@ class Order(models.Model):
     
     
 class OrderItem(models.Model):
+    class Meta:
+        ordering = ['-id']
+
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
